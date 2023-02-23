c18fb9ab4d28f80744f53cae3890b2bc0340403a
Zafrul Huzail <huzailakazaf@gmail.com>
2023-02-23 15:09:57 +0100

    without asking input from user

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ if __name__ == "__main__":
     # name = input("Enter your name: ")
     # age = input("Age:")
     # batch = input("Batch:")
-    university_name = input("University: ")
-    course_name = input("Course: ")
     
 
-student1 = student.Student(uni=university_name, course=course_name)
+student1 = student.Student(uni="hs augsburg", course="informatik")
